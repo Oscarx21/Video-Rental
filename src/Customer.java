1827038708978769982464c31247dc46425c5772
Oscar <oscarcoradi95@hotmail.com>
2025-11-26 22:22:52 -0300

    Commit 5

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,3 @@
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,6 +39,35 @@ public class Customer {
 
         result.append("Amount owed is ").append(totalAmount).append("\n");
         result.append("You earned ").append(frequentRenterPoints).append(" frequent renter points");
+
+        return result.toString();
+    }
+
+    public String htmlStatement() {
+
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+
+        StringBuilder result = new StringBuilder("<h1>Rental Record for <em>" + getName() + "</em></h1><p>\n");
+
+        for (Rental rental : rentals) {
+
+            totalAmount += rental.getCharge();
+            frequentRenterPoints += rental.getFrequentRenterPoints();
+
+            result.append(rental.getMovie().getTitle())
+                    .append(": ")
+                    .append(rental.getCharge())
+                    .append("<br>\n");
+        }
+
+        result.append("<p>You owe <em>")
+                .append(totalAmount)
+                .append("</em><p>\n");
+
+        result.append("On this rental you earned <em>")
+                .append(frequentRenterPoints)
+                .append("</em> frequent renter points<p>");
 
         return result.toString();
     }
